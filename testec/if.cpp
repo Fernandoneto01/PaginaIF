63df74c27316f71185e86f344659b781835aa42e
Fernando <112653167+Fernandoneto01@users.noreply.github.com>
2024-05-08 22:57:57 -0300

    Código do projeto

---
+++ testec/if.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h> 
+
+#include <stdlib.h> 
+
+int main(void){ 
+
+ 	int n1; 
+
+	printf("Digite o numero 1:"); 
+	scanf("%i", &n1); 
+
+	if(n1==1){ 
+		printf("Verdadeiro"); 
+	}else{
+		printf("Falso");
+	}
+
+	return 0; 
+
+} 
